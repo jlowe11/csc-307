fc472a3bfd939faf2f8e895cbfb140ef67b39ab4
Joshua Lowe <jlowe5681@gmail.com>
2024-04-19 18:40:59 -0700

    Delete Implemented

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -9,17 +9,34 @@ function MyApp() {
 
   function updateList(person) { 
     postUser(person)
-      .then(() => setCharacters([...characters, person]))
+      .then(response => {
+        if (response.status === 201) { 
+          return response.json(); 
+        }
+        throw new Error('Failed to create the user');
+      })
+      .then(addedUser => setCharacters([...characters, addedUser]))
       .catch((error) => {
         console.log(error);
       })
   }
 
   function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
-    });
-    setCharacters(updated);
+    const user = characters[index];
+    fetch(`http://localhost:8000/users/${user.id}`, {method: 'DELETE'})
+      .then(response => {
+        if (response.status === 204) { 
+          const updated = characters.filter((character, i) => i !== index);
+          setCharacters(updated); 
+        } else if (response.status === 404) {
+          console.error('User not found'); 
+        } else {
+          throw new Error('Failed to delete the user');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   }
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
