63ba0530582f7a638279556293ff05e6c6f83988
Joshua Lowe <jlowe5681@gmail.com>
2024-04-19 17:10:31 -0700

    submit button and POST call to add users

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,14 +1,18 @@
 // src/MyApp.jsx
 
-import React, { useState } from "react";
+import React, {useState, useEffect} from 'react';
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   function removeOneCharacter(index) {
@@ -20,7 +24,24 @@ function MyApp() {
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
     return promise;
-}
+  }
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, [] );
 
   return (
   <div className="container">
